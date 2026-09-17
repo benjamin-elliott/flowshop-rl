3ba5eb8baea8e39c79ba69c5ddd5bf6856aa3f7c
Benjamin Elliott <b7.elliott@connect.qut.edu.au>
2026-09-17 17:07:08 +1000

    (feat) Added evaluators module

---
+++ src/flowshoprl/evaluators.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+from flowshoprl.simulation import Simulation
+
+
+class Evaluator[T](ABC):
+    def __init__(self, sim: Simulation) -> None:
+        self.sim = sim
+
+    @abstractmethod
+    def evaluate(self) -> T: ...
+
+    @abstractmethod
+    def display(self) -> None: ...
+
+    @property
+    def _objective(self) -> T: ...
+
+class Makespan(Evaluator[float]):
+    def evaluate(self) -> float:
+        return self.sim.state.time
+
+    def display(self) -> None:
+        print('\n' + '*'*40)
+        print(f'Makespan: {self.sim.state.time}')
+        print('*'*40)
+
+    @property
+    def _objective(self) -> float:
+        return self.sim.state.time
